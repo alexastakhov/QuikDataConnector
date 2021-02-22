b8065ccfe6eff4bda6bb5c6c68676231a616a3de
Alex Astakhov <lagoon2000@mail.ru>
2021-02-22 17:17:31 +0300

    Log data to file

--- QuikDataConnectorSample/MainWindow.xaml.cs
+++ QuikDataConnectorSample/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using QDde;
 using System;
+using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading;
 using System.Windows;
 
 namespace QuikDataConnectorSample
@@ -11,6 +15,16 @@ namespace QuikDataConnectorSample
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         /// <summary>
+        /// Каталог с файлами логов двнных.
+        /// </summary>
+        private const string LogFolderName = "DataLog";
+
+        /// <summary>
+        /// Префикс файла логов с данными DDE.
+        /// </summary>
+        private const string LogFilePrefix = "DdeData";
+
+        /// <summary>
         /// DDE сервер.
         /// </summary>
         private QDde.Server ddeServer = null;
@@ -19,6 +33,31 @@ namespace QuikDataConnectorSample
         /// Состояние сервиса.
         /// </summary>
         private bool isStarted = false;
+
+        /// <summary>
+        /// Записывать ли данные в файл.
+        /// </summary>
+        private bool isSaveToFile = true;
+
+        /// <summary>
+        /// Текущее имя файла для записи логов.
+        /// </summary>
+        private string currentLogFileName;
+
+        /// <summary>
+        /// Активен ли поток записи в файл.
+        /// </summary>
+        private bool isSaveToFileActive;
+
+        /// <summary>
+        /// Поток записи логов в файл.
+        /// </summary>
+        private Thread savingLogToFileThread;
+
+        /// <summary>
+        /// Очередь сообщений для записи в лог.
+        /// </summary>
+        private ConcurrentQueue<string> logQueue = new ConcurrentQueue<string>();
 
         /// <summary>
         /// Основной конструктор.
@@ -37,6 +76,9 @@ namespace QuikDataConnectorSample
         /// </summary>
         public string ServiceName { get; set; }
 
+        /// <summary>
+        /// Запущен ли сервер.
+        /// </summary>
         public bool IsStarted
         {
             get
@@ -52,10 +94,26 @@ namespace QuikDataConnectorSample
         }
 
         /// <summary>
-        /// 
+        /// Записывать ли данные в файл.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        public bool IsSaveToFile
+        {
+            get
+            {
+                return this.isSaveToFile;
+            }
+
+            set
+            {
+                this.isSaveToFile = value;
+            }
+        }
+
+        /// <summary>
+        /// Обработка надатия кнопки Start/Stop.
+        /// </summary>
+        /// <param name="sender">Вызывающий объект.</param>
+        /// <param name="e">Параметр вызова.</param>
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(this.ServiceName))
@@ -67,6 +125,25 @@ namespace QuikDataConnectorSample
 
             if (!this.IsStarted)
             {
+                if (this.isSaveToFile)
+                {
+                    if (!Directory.Exists(LogFolderName))
+                    {
+                        Directory.CreateDirectory(LogFolderName);
+                    }
+
+                    this.logQueue = new ConcurrentQueue<string>();
+                    this.currentLogFileName = LogFilePrefix + "_" + DateTime.Now.ToString("yyyyMMdd-HHmmss");
+                    this.isSaveToFileActive = true;
+                    this.savingLogToFileThread = new Thread(new ThreadStart(SaveLogToFileThreadEntry));
+                    this.savingLogToFileThread.Start();
+                }
+                else
+                {
+                    this.savingLogToFileThread = null;
+                    this.isSaveToFileActive = false;
+                }
+
                 if (this.ddeServer == null)
                 {
                     this.ddeServer = new QDde.Server(this.ServiceName);
@@ -77,10 +154,25 @@ namespace QuikDataConnectorSample
                 this.ddeServer.SetPokeCallback(OnPoke);
                 this.IsStarted = true;
 
-                this.PrintLog($"DDE Server Server Started. Service Name : \"{this.ddeServer.ServiceName}\"");
+                this.PrintLog($"DDE server Started. Service Name : \"{this.ddeServer.ServiceName}\"");
+
+                if (this.isSaveToFile)
+                {
+                    this.PrintLog($"Received data will be written to file : \"{this.currentLogFileName}\"");
+                }
             }
             else
             {
+                if (this.savingLogToFileThread != null)
+                {
+                    this.isSaveToFileActive = false;
+
+                    if (!this.savingLogToFileThread.Join(1000))
+                    {
+                        this.savingLogToFileThread.Abort();
+                    }
+                }
+
                 this.ddeServer.Disconnect();
 
                 this.PrintLog($"DDE Server Server Stopped. Service Name : \"{this.ddeServer.ServiceName}\"");
@@ -92,14 +184,73 @@ namespace QuikDataConnectorSample
         }
 
         /// <summary>
-        /// 
+        /// Сгенерировать событие обновления свойства.
         /// </summary>
-        /// <param name="propertyName"></param>
+        /// <param name="propertyName">Имя свойства.</param>
         protected void OnPropertyChanged(string propertyName)
         {
             if (this.PropertyChanged != null)
             {
                 this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Записать данные в файл.
+        /// </summary>
+        /// <param name="service">Имя сервиса.</param>
+        /// <param name="topic">Топик сообщения.</param>
+        /// <param name="item">Область таблицы.</param>
+        /// <param name="data">Сырые данные таблицы.</param>
+        private void SaveDataToFile(string service, string topic, string item, byte[] data)
+        {
+            if (!this.isSaveToFile)
+            {
+                return;
+            }
+
+            var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var outString = $"{time} | Service: {service}, Topic: {topic} | {item} | Data: {this.BytesToString(data)}";
+
+            this.logQueue.Enqueue(outString);
+        }
+
+        /// <summary>
+        /// Преобразовать массив байт в строку.
+        /// </summary>
+        /// <param name="data">Массив байт.</param>
+        /// <returns>Строка, представляющая массив байт.</returns>
+        private string BytesToString(byte[] data)
+        {
+            StringBuilder hexString = new StringBuilder(data.Length * 2);
+
+            foreach (byte b in data)
+            {
+                hexString.AppendFormat("{0:X2}", b);
+                hexString.Append(" ");
+            }
+
+            return hexString.ToString().Trim();
+        }
+
+        /// <summary>
+        /// Нить потока записи логов в файл. 
+        /// </summary>
+        private void SaveLogToFileThreadEntry()
+        {
+            using (var sw = new StreamWriter(new FileStream(Path.Combine(LogFolderName, this.currentLogFileName), FileMode.Create)))
+            {
+                while (this.isSaveToFileActive)
+                {
+                    if (this.logQueue.TryDequeue(out var itemToWrite))
+                    {
+                        sw.WriteLine(itemToWrite);
+                    }
+
+                    Thread.Sleep(2);
+                }
+
+                sw.Flush();
             }
         }
 
@@ -173,6 +324,7 @@ namespace QuikDataConnectorSample
         /// <param name="data"></param>
         private void OnPoke(IntPtr handle, bool isPaused, string serviceName, string topic, object tag, string item, byte[] data)
         {
+            this.SaveDataToFile(serviceName, topic, item, data);
             this.PrintLog($"OnPoke : Topic={topic} Item={item} dataLen={data.Length}", LogType.DataValues);
         }
 
